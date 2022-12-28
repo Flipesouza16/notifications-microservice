e47251fe0847f66b183e4d0976f60c8d209eff85
FilipeSouza <filipe.souza1602@gmail.com>
2022-12-28 17:05:32 -0300

    create initial state of project

---
+++ src/app.controller.ts
@@ -0,0 +1,30 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { randomUUID } from 'node:crypto';
+import { CreateNotificationBody } from './create-notification-body';
+
+@Controller('notifications')
+export class AppController {
+  constructor(private readonly prisma: PrismaService) {}
+
+  @Get()
+  list() {
+    return this.prisma.notifications.findMany();
+  }
+
+  @Post()
+  async create(@Body() body: CreateNotificationBody) {
+    const { category, content, recipientId } = body
+    
+    console.log('body: ',body);
+    
+    await this.prisma.notifications.create({
+      data: {
+        id: randomUUID(),
+        content: 'Você tem uma nova solicitação de amizade!',
+        category: 'social',
+        recipientId: randomUUID()
+      }
+    });
+  }
+}
