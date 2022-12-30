b703d01db96b76edc319a872ec702dedad2cb89b
FilipeSouza <filipe.souza1602@gmail.com>
2022-12-30 17:20:47 -0300

    test: test notifications array

--- src/use-cases/send-notifications.spec.ts
+++ src/use-cases/send-notifications.spec.ts
@@ -1,15 +1,26 @@
+import { Notification } from "src/application/entities/notification";
 import { SendNotification } from "./send-notifications";
+
+const notifications: Notification[] = [];
+
+const notificationsRepository = {
+  async create(notification: Notification) {
+    notifications.push(notification);
+  }
+}
 
 describe('Send notification', () => {
   it('shoul be able to send notification', async () => {
-    const sendNotification = new SendNotification();
+    const sendNotification = new SendNotification(notificationsRepository);
 
-    const { notification } = await sendNotification.execute({
+    await sendNotification.execute({
       content: 'This is a notification',
       category: 'social',
       recipientId: 'example-recipient-id'
     })
 
-    expect(notification).toBeTruthy();
+    console.log(notifications);
+
+    expect(notifications).toHaveLength(1)
   });
 });
