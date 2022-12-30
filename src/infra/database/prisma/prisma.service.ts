b387889904a9cf030e7f96d0c896738951ef7bcf
FilipeSouza <filipe.souza1602@gmail.com>
2022-12-30 18:36:17 -0300

    feat: integrate notification use case with infra layer

--- src/infra/database/prisma/prisma.service.ts
+++ src/infra/database/prisma/prisma.service.ts
@@ -5,7 +5,7 @@ import { PrismaClient } from '@prisma/client';
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect();
-    }
+  }
 
   async enableShutdownHooks(app: INestApplication) {
     this.$on('beforeExit', async () => {
