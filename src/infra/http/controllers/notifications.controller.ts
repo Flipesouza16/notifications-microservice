90db29e60e880c7432fa4474419814719493a20a
FilipeSouza <filipe.souza1602@gmail.com>
2022-12-31 20:28:49 -0300

    refactor: create mapper to notification

--- src/infra/http/controllers/notifications.controller.ts
+++ src/infra/http/controllers/notifications.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { SendNotification } from 'src/application/use-cases/send-notifications';
 import { CreateNotificationBody } from '../dtos/create-notification-body';
+import { NotificationViewModel } from '../view-models/notificationViewModel';
 
 @Controller('notifications')
 export class NotificationsController {
@@ -16,6 +17,8 @@ export class NotificationsController {
       category
     })
 
-    return { notification }
+    return { 
+      notification: NotificationViewModel.toHttp(notification)
+    }
   }
 }
