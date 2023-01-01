3b160ed21cc1724c51c9264d5cfd0a7749de371d
FilipeSouza <filipe.souza1602@gmail.com>
2023-01-01 15:40:15 -0300

    feat: create mapper toDomain

--- src/infra/database/prisma/mappers/prisma-notification-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -1,4 +1,6 @@
+import { Notifications as RawNotification } from '@prisma/client'
 import { Notification } from "@application/entities/notification";
+import { Content } from '@application/entities/content';
 
 export class PrismaNotificationMapper {
   static toPrisma(notification: Notification) {
@@ -10,5 +12,16 @@ export class PrismaNotificationMapper {
       readAt: notification.readAt,
       createAt: notification.createAt
     }
+  }
+
+  static toDomain(raw: RawNotification): Notification {
+    return new Notification({
+      category: raw.category,
+      content: new Content(raw.content),
+      recipientId: raw.recipientId,
+      readAt: raw.canceledAt,
+      createdAt: raw.createAt
+    }, 
+    raw.id)
   }
 }
